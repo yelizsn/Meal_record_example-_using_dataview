3fbce8a56b4dc0609c23244b967bfc0d70ed685f
Yeliz <81921661+yelizsn@users.noreply.github.com>
2022-08-17 12:17:18 +0300

    Add files via upload

---
+++ DataViewOrnek2(Yemek Kayıt)/DataViewOrnek2/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DataViewOrnek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ekleBtn = new System.Windows.Forms.Button();
+            this.yemekAdi_text = new System.Windows.Forms.TextBox();
+            this.kalori_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(348, 172);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ekleBtn
+            // 
+            this.ekleBtn.Location = new System.Drawing.Point(255, 32);
+            this.ekleBtn.Name = "ekleBtn";
+            this.ekleBtn.Size = new System.Drawing.Size(85, 23);
+            this.ekleBtn.TabIndex = 1;
+            this.ekleBtn.Text = "Veri Ekle";
+            this.ekleBtn.UseVisualStyleBackColor = true;
+            this.ekleBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // yemekAdi_text
+            // 
+            this.yemekAdi_text.Location = new System.Drawing.Point(10, 34);
+            this.yemekAdi_text.Name = "yemekAdi_text";
+            this.yemekAdi_text.Size = new System.Drawing.Size(121, 20);
+            this.yemekAdi_text.TabIndex = 2;
+            // 
+            // kalori_text
+            // 
+            this.kalori_text.Location = new System.Drawing.Point(137, 34);
+            this.kalori_text.Name = "kalori_text";
+            this.kalori_text.Size = new System.Drawing.Size(111, 20);
+            this.kalori_text.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Yemek Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Kalori";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.ekleBtn);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.yemekAdi_text);
+            this.groupBox2.Controls.Add(this.kalori_text);
+            this.groupBox2.Location = new System.Drawing.Point(12, 190);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(348, 64);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Veri Ekleme İşlemi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(370, 261);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yemek Tablosu";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ekleBtn;
+        private System.Windows.Forms.TextBox yemekAdi_text;
+        private System.Windows.Forms.TextBox kalori_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ DataViewOrnek2(Yemek Kayıt)/DataViewOrnek2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataViewOrnek2
+{
+    public partial class Form1 : Form
+    {
+        OleDbDataAdapter da;
+        OleDbCommand cmd;
+        DataSet ds;
+        DataView dataView;
+        string ConnectionString = "Provider=Microsoft.ACE.Oledb.12.0;Data Source=yemek.accdb";
+   
+
+        public Form1()
+        {
+            InitializeComponent();
+            Veri_Cek();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Veri_Ekle(yemekAdi_text.Text,Convert.ToInt32(kalori_text.Text));
+        }
+        DataTable dt = new DataTable();
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        void Veri_Cek()
+        {
+            using (OleDbConnection baglanti = new OleDbConnection(ConnectionString))
+            {
+       
+                dataView = new DataView();
+                ds = new DataSet();
+                cmd = new OleDbCommand("SELECT * FROM Yemek", baglanti);
+                da = new OleDbDataAdapter();
+                da.SelectCommand = cmd;
+                da.Fill(ds, "Add New");
+                dataView = new DataView(ds.Tables[0]);
+                dataView.Sort = "Id";
+                dataGridView1.DataSource = dataView;
+
+            }
+        }
+
+        void Veri_Ekle(string yemek_adi, int kalori)
+        {
+            try
+            {
+                DataRowView newRow = dataView.AddNew();
+                newRow["Yemek_Adi"] = yemek_adi;
+                newRow["Kalori"] = kalori;
+                newRow.EndEdit();
+                dataView.Sort = "Id";
+                OleDbConnection baglanti = new OleDbConnection(ConnectionString);
+                OleDbCommand com = new OleDbCommand("INSERT INTO Yemek (Yemek_Adi,Kalori) VALUES (@Yemek_Adi,@Kalori)");
+                com.Connection = baglanti;
+                baglanti.Open();
+                com.Parameters.Add("@Yemek_Adi", OleDbType.VarChar).Value = dataView[0][1];
+                com.Parameters.Add("@Kalori", OleDbType.Integer).Value = dataView[0][2];
+                try
+                {
+                    com.ExecuteNonQuery();  
+                    MessageBox.Show("Yemek Ekleme İşlemi Başarılı!");
+                    baglanti.Close();
+                    Veri_Cek(); 
+                }
+                catch (OleDbException ex)
+                {
+                    MessageBox.Show(ex.Source);
+                    baglanti.Close();
+                
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
